477b75aa82e5d87a6d9ccbdde818c54f0b07a53a
Olga Lapchuk <lapchuk.olga@gmail.com>
2022-09-27 13:41:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('Are you here');
+//console.log = ('confirm');
+
+//const anwser = prompt("Вaм есть 18?", "");
+//console.log(typeof(promt));
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя', '');
+//answers[1] = prompt('Как ваша фамилия', '');
+//answers[2] = prompt('Сколько Вам лет', '');
+
+const category = 'toys';
+
+console.log('https');
